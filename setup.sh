dbb7f6870203ddfc8a99d886a4cd5de59f18a3c1
LowK <davidkevinit@gmail.com>
2022-08-27 14:36:18 +0700

    fixed check download

--- setup.sh
+++ setup.sh
@@ -21,17 +21,10 @@ if [ $countFiles -ge 1 ]; then
 fi
 
 # Continue if not download yet
-if [ ${#GITHUB_TOKEN[@]} -eq 0 ]; then
+if [ ${#GITHUB_TOKEN} -eq 0 -eq 0 ]; then
     read -p "Enter a Github key: " GITHUB_TOKEN
     export GITHUB_TOKEN=$GITHUB_TOKEN
 fi
 
 curl -sL -H "Accept: application/vnd.github.v4.raw" -H "Authorization: token $GITHUB_TOKEN" -o "$workingDir/persistent.sh" "$github_api/persistent.sh?ref=$branch"
 
-if [ $(grep -E "Bad credentials|Not Found" "$workingDir/persistent.sh" | wc -l) -eq 1 ]; then
-    echo "Cannot download the file: persistent.sh"
-    rm "$workingDir/persistent.sh"
-else 
-    echo "Downloaded the file: persistent.sh"
-    eval "bash $workingDir/persistent.sh"
-fi
