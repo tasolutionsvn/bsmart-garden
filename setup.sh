e5bf073bc39dd05710eda7f8f80fe66bb065d90b
LowK <davidkevinit@gmail.com>
2022-08-27 13:27:37 +0700

    fixed

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 GITHUB_TOKEN="${GITHUB_TOKEN:-}"
 branch="master"
-github_api="https://github.com/tasolutionsvn/activation/contents"
+github_api="https://api.github.com/repos/tasolutionsvn/activation/contents"
 
 workingDir="$(pwd)";
 if [ "$1" ]; then
@@ -14,21 +14,21 @@ if [ -z $GITHUB_TOKEN ]; then
     read -p "Enter a Github key: " GITHUB_TOKEN
 fi
 
-countFiles=$(ls -l "$workingDir/setup.sh" 2>/dev/null | wc -l)
+countFiles=$(ls -l "$workingDir/persistent.sh" 2>/dev/null | wc -l)
 if [ $countFiles -ge 1 ]; then
-    echo 'Downloaded setup.sh'
+    echo 'Downloaded persistent.sh'
     return
 fi
 
 # Continue if not download yet
-if [ -z $GITHUB_TOKEN ]; then
+if [ ${#GITHUB_TOKEN[@]} -eq 0 ]; then
     read -p "Enter a Github key: " GITHUB_TOKEN
     export GITHUB_TOKEN=$GITHUB_TOKEN
 fi
 
 curl -sL -H "Accept: application/vnd.github.v4.raw" -H "Authorization: token $GITHUB_TOKEN" -o "$workingDir/persistent.sh" "$github_api/persistent.sh?ref=$branch"
 
-if [ $(grep "Bad credentials" "$workingDir/persistent.sh" | wc -l) -eq 1 ]; then
+if [ $(grep -E "Bad credentials|Not Found" "$workingDir/persistent.sh" | wc -l) -eq 1 ]; then
     echo "Cannot download the file: persistent.sh"
     rm "$workingDir/persistent.sh"
 else 
